575531d10fcc23ceb69e782b90df32af032eb568
Edison Cristovão <edison.cr.jr@gmail.com>
2023-08-14 21:19:11 -0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex justify-center items-center text-white grow">
-      <div className="mr-40">
+    <div className="flex md:flex-row flex-col-reverse justify-between items-center text-white grow mx-10 my-5 md:my-0">
+      <div>
         <div className="mb-10">
           <p className="">Hi all, I am</p>
-          <h1 className="ml-[-8px] my-[-10px]">Edison Cristovao</h1>
+          <h1 className="ml-[-8px] my-[-10px]">Edison Cristovão</h1>
           <h2 className="text-secondary-blue">{"> Full-stack develope"}r</h2>
         </div>
 
         <div>
-          <h3>{`// complete the game to continue`}</h3>
-          <h3>{`// you can also see it on my Github page`}</h3>
+          {/* <h3>{`// complete the game to continue`}</h3> */}
+          <h3>{`// you can also see it on my Github`}</h3>
           <h3>
             <span className="text-secondary-blue">const</span>{" "}
             <span className="text-accent-green"> githubLink</span>
             {" = "}
-            <span className="text-accent-nude">
-              “https://github.com/example/url”
-            </span>
+            <Link href={"https://github.com/EdisonCristovao"} target="_blank">
+              <span className="text-accent-nude hover:underline">
+                “https://github.com/EdisonCristovao”
+              </span>
+            </Link>
           </h3>
         </div>
       </div>
 
-      <div>
-        <div className="h-56 w-56 bg-black"></div>
+      <div className="my-5 md:my-0">
+        {/* <div className="h-56 w-56 bg-black"></div> */}
+        <Image
+          src={"/imgs/profile-photo.png"}
+          width={500}
+          height={200}
+          alt=""
+        />
       </div>
     </div>
   );
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "./Link";
+import Image from "next/image";
 
 export default function Nav() {
   return (
     <nav className="flex items-center w-full h-14 bg-primary text-secondary-grey border-collapse border border-lines">
       <div className="w-64 pl-6">
-        <label>EC.codes</label>
+        <Image src={"/imgs/logo.svg"} alt="" width={80} height={800} />
+        {/* <label>EC.codes</label> */}
       </div>
 
       <Link label="_home" href={"/"}></Link>
