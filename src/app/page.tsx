0b9255c50d0f330703a1e26e6c5ec00d07f12edd
Edison Cristovão <edison.cr.jr@gmail.com>
2023-08-17 19:50:40 -0300

    home page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex md:flex-row flex-col-reverse justify-between md:justify-center items-center text-white grow mx-10 my-5 md:my-0">
-      <div>
+    <div className="flex flex-col md:flex-row  md:justify-center items-center text-white grow mx-10 my-5 md:my-0">
+      <div className="flex flex-col justify-around h-[80%] md:h-0">
         <div className="mb-10">
-          <p className="text-base md:text-lg">Hi all, I am</p>
-          <h1 className="text-5xl md:text-6xl ml-[-8px] my-[-8px]">
+          <p className="text-lg md:text-lg">Hi all, I am</p>
+          <h1 className="text-6xl md:text-7xl ml-[-8px] my-[-8px]">
             Edison Cristovão
           </h1>
           <h2 className="text-base md:text-3xl text-secondary-blue">
@@ -17,21 +17,26 @@ export default function Home() {
 
         <div>
           {/* <h3>{`// complete the game to continue`}</h3> */}
-          <h3 className="text-xs md:text-base">{`// you can also see it on my Github`}</h3>
-          <h3 className="text-xs md:text-base">
-            <span className="text-secondary-blue">const</span>{" "}
-            <span className="text-accent-green"> githubLink</span>
+          <h3 className="text-sm md:text-base">{`// you can also see it on my Github`}</h3>
+          <div>
+            <span className="text-secondary-blue text-sm md:text-base">
+              const
+            </span>{" "}
+            <span className="text-accent-green text-sm md:text-base">
+              {" "}
+              githubLink
+            </span>
             {" = "}
             <Link href={"https://github.com/EdisonCristovao"} target="_blank">
-              <span className="text-accent-nude hover:underline">
+              <span className="text-accent-nude hover:underline text-sm md:text-base">
                 “https://github.com/EdisonCristovao”
               </span>
             </Link>
-          </h3>
+          </div>
         </div>
       </div>
 
-      <div className="my-5 md:my-0">
+      <div className="hidden md:inline">
         {/* <div className="h-56 w-56 bg-black"></div> */}
         <Image
           src={"/imgs/profile-photo.png"}
