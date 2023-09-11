a9a39044188a5e1f5bc263df891f9b9bdf29fadc
Edison Cristovão <edison.cr.jr@gmail.com>
2023-09-10 21:48:07 -0300

    fix erro footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,11 @@ import Link from "./Link";
 export default function Footer() {
   return (
     <nav className="flex items-center w-full bg-primary text-secondary-grey border-collapse border border-lines h-14">
-      <Link href="" label="Find me in:"></Link>
+      {/* <Link href="/" label="Find me in:"></Link>
 
-      <Link href="" label="T"></Link>
-      <Link href="" label="Int"></Link>
-      <Link href="" label="F"></Link>
+      <Link href="/" label="T"></Link>
+      <Link href="/" label="Int"></Link>
+      <Link href="/" label="F"></Link> */}
     </nav>
   );
 }
